e4defba5a42ce6280790770d45884db5faa02c81
David Gajda <dgaja23@hotmail.com>
2024-10-28 16:59:36 +0100

    Initial Commit

---
+++ stock-tracker/stock_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  stock_trackerApp.swift
+//  stock-tracker
+//
+//  Created by David Gajda on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct stock_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ stock-trackerTests/stock_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  stock_trackerTests.swift
+//  stock-trackerTests
+//
+//  Created by David Gajda on 10/28/24.
+//
+
+import Testing
+@testable import stock_tracker
+
+struct stock_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
